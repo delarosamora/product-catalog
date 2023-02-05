33baf94b1d2f12ccf0c8ce1b189f9b35712035e7
Alvaro de la Rosa <alvarodelarosamora@gmail.com>
2023-02-05 17:47:57 +0100

    Refactorizar product-card y product-page

--- resources/views/products/include/product-card.blade.php
+++ resources/views/products/include/product-card.blade.php
@@ -1,9 +1,9 @@
 <li class="col product-card" data-name="{{ $product->name }}" data-category="{{ $product->category_id }}">
     <div class="card m-auto border border-3 shadow-lg h-100 rounded-5 overflow-hidden">
-        <img src="/storage/products/{{ $product->safe_image }}" class="card-img-top" alt="..." style="height: 20vh; object-fit: cover">
+        <img src="/storage/products/{{ $product->safe_image }}" class="card-img-top object-fit-cover" alt="..." style="height: 20vh">
         <div class="card-body text-center">
           <a href="{{ route('products.show', ['product' => $product]) }}" class="stretched-link">
-            <h5 class="card-title">{{ $product->name }}</h5>     
+            <h5 class="card-title text-truncate">{{ Str::upper($product->name) }}</h5>     
           </a>
           <p class="text-start"><span class="badge text-bg-primary">{{ $product->code_tag }}</span></p>
           <h6 class="text-start card-subtitle mb-2 text-muted">{{ $product->category->name }}</h6>
--- resources/views/products/include/product-page.blade.php
+++ resources/views/products/include/product-page.blade.php
@@ -3,7 +3,7 @@
     <img class="img-fluid img-thumbnail w-75" style="max-height: 60vh; object-fit: contain" src="/storage/products/{{ $product->safe_image }}" />
   </div>
   <div class="col p-2">
-    <h1 class="text-center">{{ $product->name }}</h1>
+    <h1 class="text-center">{{ Str::upper($product->name) }}</h1>
     <h3 class="text-center text-muted">{{ $product->category->name }}</h3>
     <h4 class="text-center">{{ $product->price }} €</h4>
     <h5 class="text-center"><span class="badge text-bg-primary">{{ $product->code_tag }}</span></h5>
